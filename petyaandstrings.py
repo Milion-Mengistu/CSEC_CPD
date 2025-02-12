995bfc881ffc13c87487a6e5fb5838cb3f723870
Million-Mengistu <144734295+Mili-21@users.noreply.github.com>
2025-02-12 15:16:52 +0300

    x

---
+++ petyaandstrings.py
@@ -0,0 +1,10 @@
+def petyaandstrings():
+    a = input().lower()
+    b = input().lower()
+    if a < b:
+        print(-1)
+    elif a > b:
+        print(1)
+    else:
+        print(0)
+petyaandstrings()
